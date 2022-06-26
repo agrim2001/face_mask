c644cc30105bcdebf9134a86484ad916fae4e49b
agrim2001 <61651315+agrim2001@users.noreply.github.com>
2022-06-26 22:48:06 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+import uvicorn
+from fastapi import FastAPI, File, UploadFile
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+
+app_desc = """<h2></h2>
+<br>by Agrim gupta"""
+
+app = FastAPI(title='mask api', description=app_desc)
+
+origins = [
+    "http://localhost:3000",
+    "localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+MODEL = tf.keras.models.load_model("./models/1")
+
+CLASS_NAMES = ["Mask", "No mask"]
+
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+
+@app.get("/", include_in_schema=False)
+async def index():
+    return RedirectResponse(url="/docs")
+
+
+@app.post("/predict/image")
+async def predict_api(
+        file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    img_batch = np.expand_dims(image, 0)
+    predictions = MODEL.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    confidence = round((confidence * 100),2)
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="localhost")
